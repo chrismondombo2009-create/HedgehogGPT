5351d8080a18f6c55e01715a4e480e9982b69fa5
HkrUchiwa2011 <fadilbouko780@gmail.com>
2026-01-02 07:32:20 +0100

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -1,98 +1,271 @@
 const { getTime, drive } = global.utils;
+const Canvas = require("canvas");
+const fs = require("fs-extra");
+const path = require("path");
 
 module.exports = {
-        config: {
-                name: "leave",
-                version: "1.4",
-                author: "NTKhang",
-                category: "events"
-        },
+    config: {
+        name: "leave",
+        version: "2.0",
+        author: "NTKhang & patched by 𝐋'𝐔𝐜𝐡𝐢𝐡𝐚 𝐏𝐞𝐫𝐝𝐮",
+        category: "events"
+    },
 
-        langs: {
-                vi: {
-                        session1: "sáng",
-                        session2: "trưa",
-                        session3: "chiều",
-                        session4: "tối",
-                        leaveType1: "tự rời",
-                        leaveType2: "bị kick",
-                        defaultLeaveMessage: "{userName} đã {type} khỏi nhóm"
-                },
-                en: {
-                        session1: "morning",
-                        session2: "noon",
-                        session3: "afternoon",
-                        session4: "evening",
-                        leaveType1: "𝐞𝐬𝐭 𝐞𝐧𝐟𝐢𝐧 𝐩𝐚𝐫𝐭𝐢 𝐝'𝐢𝐜𝐢",
-                        leaveType2: "𝐞𝐬𝐭 𝐛𝐚𝐧𝐧𝐢 𝐝𝐮 𝐠𝐫𝐨𝐮𝐩𝐞",
-                        defaultLeaveMessage: "-１...𝐡𝐦𝐦 ❤️‍🩹{userName}❤️‍🩹 {type} 𝐢𝐥 𝐬𝐞𝐫𝐯𝐚𝐢𝐭 𝐚 𝐫𝐢𝐞𝐧 𝐝'𝐭𝐨𝐮𝐭𝐞 𝐟𝐚𝐜𝐨𝐧...🩸🥱"
-                }
-        },
-
-        onStart: async ({ threadsData, message, event, api, usersData, getLang }) => {
-                if (event.logMessageType == "log:unsubscribe")
-                        return async function () {
-                                const { threadID } = event;
-                                const threadData = await threadsData.get(threadID);
-                                if (!threadData.settings.sendLeaveMessage)
-                                        return;
-                                const { leftParticipantFbId } = event.logMessageData;
-                                if (leftParticipantFbId == api.getCurrentUserID())
-                                        return;
-                                const hours = getTime("HH");
-
-                                const threadName = threadData.threadName;
-                                const userName = await usersData.getName(leftParticipantFbId);
-
-                                // {userName}   : name of the user who left the group
-                                // {type}       : type of the message (leave)
-                                // {boxName}    : name of the box
-                                // {threadName} : name of the box
-                                // {time}       : time
-                                // {session}    : session
-
-                                let { leaveMessage = getLang("defaultLeaveMessage") } = threadData.data;
-                                const form = {
-                                        mentions: leaveMessage.match(/\{userNameTag\}/g) ? [{
-                                                tag: userName,
-                                                id: leftParticipantFbId
-                                        }] : null
-                                };
-
-                                leaveMessage = leaveMessage
-                                        .replace(/\{userName\}|\{userNameTag\}/g, userName)
-                                        .replace(/\{type\}/g, leftParticipantFbId == event.author ? getLang("leaveType1") : getLang("leaveType2"))
-                                        .replace(/\{threadName\}|\{boxName\}/g, threadName)
-                                        .replace(/\{time\}/g, hours)
-                                        .replace(/\{session\}/g, hours <= 10 ?
-                                                getLang("session1") :
-                                                hours <= 12 ?
-                                                        getLang("session2") :
-                                                        hours <= 18 ?
-                                                                getLang("session3") :
-                                                                getLang("session4")
-                                        );
-
-                                form.body = leaveMessage;
-
-                                if (leaveMessage.includes("{userNameTag}")) {
-                                        form.mentions = [{
-                                                id: leftParticipantFbId,
-                                                tag: userName
-                                        }];
-                                }
-
-                                if (threadData.data.leaveAttachment) {
-                                        const files = threadData.data.leaveAttachment;
-                                        const attachments = files.reduce((acc, file) => {
-                                                acc.push(drive.getFile(file, "stream"));
-                                                return acc;
-                                        }, []);
-                                        form.attachment = (await Promise.allSettled(attachments))
-                                                .filter(({ status }) => status == "fulfilled")
-                                                .map(({ value }) => value);
-                                }
-                                message.send(form);
-                        };
+    langs: {
+        fr: {
+            session1: "matin",
+            session2: "midi", 
+            session3: "après-midi",
+            session4: "soir",
+            leaveType1: "est enfin parti d'ici",
+            leaveType2: "est banni du groupe",
+            funnyMessagesKicked: [
+                "{userName} vient de prendre un aller simple pour la sortie 🗿",
+                "{userName} s'est fait montrer la porte 😩",
+                "{userName} vient de goûter aux chaussures de l'admin 🧎",
+                "Direction la corbeille pour {userName} 🥱",
+                "{userName} a été éjecté du cirque 😩",
+                "On a balayé {userName} comme une vieille poussière 🗿",
+                "{userName} a testé les limites 🧎",
+                "{userName} a explosé en vol 🥱",
+                "Coup de pied aux fesses pour {userName} 😩",
+                "{userName} a reçu son billet de sortie 💔😹"
+            ],
+            funnyMessagesLeft: [
+                "{userName} a pris ses jambes à son cou 🗿",
+                "{userName} s'est évaporé comme un pet dans le vent 🥱",
+                "{userName} a claqué la porte 😩",
+                "{userName} a lâché prise 🧎",
+                "{userName} a fait ses valises 🗿",
+                "{userName} aspiré par un vortex d'incompétence 😩",
+                "{userName} a trouvé un trou 🧎",
+                "{userName} a perdu le nord 🥱",
+                "{userName} a quitté la scène 💔😹",
+                "{userName} s'est envolé comme une feuille morte 🗿"
+            ]
         }
+    },
+
+    onStart: async ({ threadsData, message, event, api, usersData, getLang }) => {
+        if (event.logMessageType == "log:unsubscribe")
+            return async function () {
+                const { threadID } = event;
+                const threadData = await threadsData.get(threadID);
+                if (!threadData.settings.sendLeaveMessage)
+                    return;
+                const { leftParticipantFbId } = event.logMessageData;
+                if (leftParticipantFbId == api.getCurrentUserID())
+                    return;
+                const hours = getTime("HH");
+
+                const threadName = threadData.threadName;
+                const userName = await usersData.getName(leftParticipantFbId);
+                const isKicked = leftParticipantFbId !== event.author;
+
+                const funnyMessages = isKicked ? getLang("funnyMessagesKicked") : getLang("funnyMessagesLeft");
+                const randomFunnyMessage = funnyMessages[Math.floor(Math.random() * funnyMessages.length)];
+                
+                let leaveMessage = randomFunnyMessage.replace(/\{userName\}/g, userName);
+
+                const form = {
+                    mentions: [{
+                        tag: userName,
+                        id: leftParticipantFbId
+                    }]
+                };
+
+                leaveMessage = leaveMessage
+                    .replace(/\{threadName\}/g, threadName)
+                    .replace(/\{time\}/g, hours)
+                    .replace(/\{session\}/g, hours <= 10 ?
+                        getLang("session1") :
+                        hours <= 12 ?
+                            getLang("session2") :
+                            hours <= 18 ?
+                                getLang("session3") :
+                                getLang("session4")
+                    );
+
+                form.body = "◆ ▬▬▬▬ ❴✪❵ ▬▬▬▬ ◆\n\n" + leaveMessage + "\n\n◆ ▬▬▬▬ ❴✪❵ ▬▬▬▬ ◆";
+
+                try {
+                    const threadInfo = await api.getThreadInfo(threadID);
+                    const membersLeft = threadInfo.participantIDs.length;
+                    
+                    const userAvatar = await this.getUserAvatar(api, leftParticipantFbId);
+                    const groupAvatar = await this.getGroupAvatar(api, threadID);
+                    
+                    const leaveImage = await this.createLeaveCanvas(
+                        userName, 
+                        userAvatar, 
+                        threadName, 
+                        groupAvatar, 
+                        membersLeft,
+                        isKicked ? "kicked" : "left",
+                        leaveMessage
+                    );
+                    
+                    const imagePath = path.join(__dirname, `leave_${leftParticipantFbId}_${Date.now()}.png`);
+                    fs.writeFileSync(imagePath, leaveImage);
+
+                    await message.send(form);
+                    await message.send({ 
+                        body: " ",
+                        attachment: fs.createReadStream(imagePath) 
+                    });
+
+                    fs.unlinkSync(imagePath);
+
+                } catch (err) {
+                    console.error("Erreur leave event:", err);
+                    if (threadData.data.leaveAttachment) {
+                        const files = threadData.data.leaveAttachment;
+                        const attachments = files.reduce((acc, file) => {
+                            acc.push(drive.getFile(file, "stream"));
+                            return acc;
+                        }, []);
+                        form.attachment = (await Promise.allSettled(attachments))
+                            .filter(({ status }) => status == "fulfilled")
+                            .map(({ value }) => value);
+                    }
+                    message.send(form);
+                }
+            }.bind(this);
+    },
+
+    getUserAvatar: async function (api, userID) {
+        try {
+            const userInfo = await api.getUserInfo(userID);
+            if (userInfo[userID] && userInfo[userID].thumbSrc) {
+                const response = await global.utils.getStreamFromURL(userInfo[userID].thumbSrc);
+                return await this.bufferFromStream(response);
+            }
+        } catch {}
+        return null;
+    },
+
+    getGroupAvatar: async function (api, threadID) {
+        try {
+            const photos = await api.getThreadPictures(threadID, 0, 1);
+            if (photos && photos.length > 0 && photos[0].uri) {
+                const response = await global.utils.getStreamFromURL(photos[0].uri);
+                return await this.bufferFromStream(response);
+            }
+            
+            const threadInfo = await api.getThreadInfo(threadID);
+            if (threadInfo.imageSrc) {
+                const response = await global.utils.getStreamFromURL(threadInfo.imageSrc);
+                return await this.bufferFromStream(response);
+            }
+        } catch {}
+        return null;
+    },
+
+    bufferFromStream: async function (stream) {
+        return new Promise((resolve) => {
+            const chunks = [];
+            stream.on("data", (chunk) => chunks.push(chunk));
+            stream.on("end", () => resolve(Buffer.concat(chunks)));
+            stream.on("error", () => resolve(null));
+        });
+    },
+
+    createLeaveCanvas: async function (userName, userAvatar, groupName, groupAvatar, membersLeft, type, funnyText) {
+        const W = 1200, H = 900;
+        const canvas = Canvas.createCanvas(W, H);
+        const ctx = canvas.getContext("2d");
+
+        const grd = ctx.createLinearGradient(0, 0, W, H);
+        if (type === "left") {
+            grd.addColorStop(0, "#8B0000");
+            grd.addColorStop(1, "#FF6347");
+        } else {
+            grd.addColorStop(0, "#4A0000");
+            grd.addColorStop(1, "#8B0000");
+        }
+        ctx.fillStyle = grd;
+        ctx.fillRect(0, 0, W, H);
+
+        if (groupAvatar) {
+            try {
+                const groupImg = await Canvas.loadImage(groupAvatar);
+                ctx.save();
+                ctx.beginPath();
+                ctx.arc(100, 100, 40, 0, Math.PI * 2);
+                ctx.closePath();
+                ctx.clip();
+                ctx.drawImage(groupImg, 60, 60, 80, 80);
+                ctx.restore();
+            } catch {}
+        }
+
+        ctx.font = "bold 28px Arial";
+        ctx.fillStyle = "#FFF";
+        ctx.textAlign = "left";
+        ctx.fillText(groupName, 160, 120);
+
+        ctx.font = "bold 65px Arial";
+        ctx.textAlign = "center";
+        ctx.fillStyle = type === "left" ? "#FFA500" : "#FF0000";
+        ctx.fillText(type === "left" ? "DÉPART" : "EXPULSION", W / 2, 220);
+
+        if (userAvatar) {
+            try {
+                const userImg = await Canvas.loadImage(userAvatar);
+                ctx.save();
+                ctx.beginPath();
+                ctx.arc(W / 2, 380, 100, 0, Math.PI * 2);
+                ctx.closePath();
+                ctx.clip();
+                ctx.drawImage(userImg, W / 2 - 100, 280, 200, 200);
+                ctx.restore();
+            } catch {}
+        }
+
+        ctx.font = "bold 38px Arial";
+        ctx.fillStyle = "#FFD700";
+        ctx.fillText(userName, W / 2, 520);
+
+        ctx.font = "26px Arial";
+        ctx.fillStyle = "rgba(255,255,255,0.9)";
+        
+        const funnyLines = this.wrapText(ctx, funnyText, W - 100, 26);
+        funnyLines.forEach((line, i) => {
+            ctx.fillText(line, W / 2, 580 + i * 40);
+        });
+
+        ctx.font = "bold 32px Arial";
+        ctx.fillStyle = "#00FFAA";
+        ctx.fillText(`Membres restants : ${membersLeft}`, W / 2, 730);
+
+        ctx.fillStyle = "rgba(0,0,0,0.3)";
+        ctx.fillRect(0, H - 80, W, 80);
+        
+        ctx.fillStyle = "#AAAAAA";
+        ctx.font = "22px Arial";
+        ctx.textAlign = "center";
+        ctx.fillText("Fin de l'aventure", W / 2, H - 35);
+
+        return canvas.toBuffer();
+    },
+
+    wrapText: function (ctx, text, maxWidth, fontSize) {
+        ctx.font = `${fontSize}px Arial`;
+        const words = text.split(' ');
+        const lines = [];
+        let currentLine = words[0];
+
+        for (let i = 1; i < words.length; i++) {
+            const word = words[i];
+            const width = ctx.measureText(currentLine + " " + word).width;
+            if (width < maxWidth) {
+                currentLine += " " + word;
+            } else {
+                lines.push(currentLine);
+                currentLine = word;
+            }
+        }
+        lines.push(currentLine);
+        return lines;
+    }
 };
