13cdc03dbc7acf0434e0e774d29721cecadeba6a
Hedgehog03 <botastro39@gmail.com>
2026-03-19 06:47:13 +0100

    Update withelist.js

--- scripts/cmds/withelist.js
+++ scripts/cmds/withelist.js
@@ -33,7 +33,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, usersData, event, getLang, api }) {
-    const permission = ["100083846212138", " 61578433048588"];
+    const permission = ["100083846212138", "61578433048588"];
     if (!permission.includes(event.senderID)) {
       api.sendMessage(
         "Oh merde 🐥 \nT'essaies de jouer au maître? Ducon tu ne peux pas utliser cette commande.\nTrou du cul. Reviens l'utiliser quand tu sauras compter.",
