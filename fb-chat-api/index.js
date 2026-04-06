2fbb152dd710e931418dc698b2f10ac0e55b7756
HkrUchiwa2011 <fadilbouko780@gmail.com>
2026-04-06 07:57:07 +0100

    Update index.js

--- fb-chat-api/index.js
+++ fb-chat-api/index.js
@@ -431,7 +431,7 @@ function login(loginData, options, callback) {
         autoMarkRead: true,
         autoReconnect: true,
         logRecordSize: 100,
-        online: trye,
+        online: true,
         emitReady: false,
         userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
     };
