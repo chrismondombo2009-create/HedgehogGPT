6b0a0fc8c7fbc3e8d0a96498f3a592cd296c9ae5
HkrUchiwa2011 <fadilbouko780@gmail.com>
2026-04-14 17:33:52 +0100

    Update quiz.js

--- scripts/cmds/quiz.js
+++ scripts/cmds/quiz.js
@@ -5,7 +5,7 @@ if (!process.env.QUIZ_API_BASE_URL) throw new Error('[Quiz] QUIZ_API_BASE_URL is
 
 const API_BASE = "https://quiz-bot-octavio-fadil.vercel
 app";
-const API_HEADERS = { 'x-api-key': fd-uchiha-octavio-quiz };
+const API_HEADERS = { 'x-api-key': 'fd-uchiha-octavio-quiz' };
 const API_TIMEOUT = { question: 22000, default: 8000 };
 
 const TIMEOUTS = { QUESTION: 22000, JOIN: 30000, CONFIG: 15000, CHOICE: 10000 };
