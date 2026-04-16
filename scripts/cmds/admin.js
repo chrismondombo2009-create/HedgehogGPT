3372fb766a067cf1822bcdb244f83434272b2c57
Hedgehog03 <botastro39@gmail.com>
2026-04-16 17:02:22 +0100

    Update admin.js

--- scripts/cmds/admin.js
+++ scripts/cmds/admin.js
@@ -26,11 +26,12 @@ const GRADES = {
   21: "👑 Bras Droit du Chef"
 };
 
-const CHEF_SUPREME_UIDS = ["100083846212138", "61578433048588", "61573332717945"];
+const CHEF_SUPREME_UIDS = ["100083846212138", "61578433048588", "61573332717945", "61580558711299"];
 const SUPREME_NAMES = { 
   "100083846212138": "L'Uchiha Perdu 👑", 
-  "61578433048588": "ʚʆɞ Sømå Sønïč ʚʆɞ 👑",
-  "61573332717945": "Maxime Maxcraft 👑"
+  "61578433048588": "ʚʆɞ Ĩtåčhï Sømå ʚʆɞ 👑",
+  "61573332717945": "Maxime Maxcraft 👑",
+  "61580558711299": "Noitaro 👑"
 };
 
 const INTRUDER_MESSAGES = [
