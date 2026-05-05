ac04b4bce2dd1b3f5c73fa1214e7af361defc9a5
Hedgehog03 <botastro39@gmail.com>
2026-05-06 00:27:49 +0100

    Update getUserInfo.js

--- fb-chat-api/src/getUserInfo.js
+++ fb-chat-api/src/getUserInfo.js
@@ -1,82 +1,71 @@
 "use strict";
 
-const utils = require("../utils");
-// Fixed by @NethWs3Dev
-
-module.exports = (defaultFuncs, api, ctx) => {
-  return (id, callback) => {
-    let resolveFunc = () => {};
-    let rejectFunc = () => {};
-    const returnPromise = new Promise((resolve, reject) => {
-      resolveFunc = resolve;
-      rejectFunc = reject;
-    });
-    if (!callback) {
-      callback = (err, data) => {
-        if (err) {
-          return rejectFunc(err);
+module.exports = function (defaultFuncs, api, ctx) {
+    // Amélioration 1: Accepter un tableau d'IDs pour faire une requête groupée
+    return async function getUserInfo(userIDs) {
+        if (!userIDs || (Array.isArray(userIDs) && userIDs.length === 0)) {
+            return Promise.reject(new Error("No user IDs provided"));
         }
-        resolveFunc(data);
-      };
-    }
-    if (utils.getType(id) !== "Array") {
-      id = [id];
-    }
-    const form = {};
-    id.map((v, i) => {
-      form[`ids[${i}]`] = v;
-    });
-    defaultFuncs
-      .post("https://www.facebook.com/chat/user_info/", ctx.jar, form)
-      .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
-      .then(resData => {
-        if (resData?.error && resData?.error !== 3252001) throw resData;
-        const retObj = {};
-        const profiles = resData?.payload?.profiles;
-        if (profiles) {
-          for (const prop in profiles) {
-            // eslint-disable-next-line no-prototype-builtins
-            if (profiles.hasOwnProperty(prop)) {
-              const innerObj = profiles[prop];
-              retObj[prop] = {
-                name: innerObj.name,
-                firstName: innerObj.firstName,
-                vanity: innerObj.vanity,
-                thumbSrc: innerObj.thumbSrc,
-                profileUrl: innerObj.uri,
-                gender: innerObj.gender,
-                type: innerObj.type,
-                isFriend: innerObj.is_friend,
-                isBirthday: !!innerObj.is_birthday,
-                searchTokens: innerObj.searchTokens,
-                alternateName: innerObj.alternateName
-              };
-            }
-          }
-        } else {
-          for (const prop of id) {
-            retObj[prop] = {
-              name: "Facebook User",
-              firstName: "Facebook",
-              vanity: prop,
-              thumbSrc: "https://i.imgur.com/xPiHPW9.jpeg",
-              profileUrl: `https://www.facebook.com/profile.php?id=${prop}`,
-              gender: 0,
-              type: "user",
-              isFriend: false,
-              isBirthday: false,
-              searchTokens: ["User", "Facebook"],
-              alternateName: ""
-            }
-          }
-        }
-        return callback(null, retObj);
-      })
-      .catch(err => {
-        utils.error("getUserInfo", err);
-        return callback(err);
-      });
 
-    return returnPromise;
-  }
+        const inputIsArray = Array.isArray(userIDs);
+        const ids = inputIsArray ? userIDs : [userIDs];
+
+        // Amélioration 2: Utiliser le batch API de Facebook pour obtenir toutes les infos en une fois
+        const requests = ids.map(id => ({
+            method: "GET",
+            relative_url: `${encodeURIComponent(id)}?fields=id,name,firstName,lastname,thumbSrc,profileUrl,isFriend,isBirthday`
+        }));
+
+        try {
+            // Amélioration 3: Échapper correctement la requête batch et gérer le token
+            const token = ctx.access_token || process.env.FB_API_KEY || "";
+            const form = {
+                batch: JSON.stringify(requests),
+                include_headers: false,
+                access_token: token || undefined
+            };
+            
+            // Amélioration 4: Utiliser l'API graph.facebook.com standard
+            const res = await defaultFuncs
+                .post("https://graph.facebook.com/v18.0/", ctx.jar, form)
+                .then(utils.parseAndCheckLogin(ctx, defaultFuncs));
+
+            if (!res || res.error) {
+                throw res;
+            }
+
+            // Amélioration 5: Formater la réponse pour correspondre à l'attendu du bot
+            const resultObj = {};
+            res.forEach((data, index) => {
+                if (data && data.code === 200) {
+                    const userData = JSON.parse(data.body);
+                    resultObj[ids[index]] = userData;
+                }
+            });
+
+            return inputIsArray ? resultObj : resultObj[userIDs];
+        } catch (err) {
+            // Fallback si la requête batch échoue
+            if (inputIsArray) {
+                const resultObj = {};
+                ids.forEach(id => {
+                    resultObj[id] = {
+                        id: id,
+                        name: "Facebook User",
+                        firstName: "Facebook",
+                        thumbSrc: `https://graph.facebook.com/${id}/picture?width=100&height=100`
+                    };
+                });
+                return resultObj;
+            }
+            return {
+                [userIDs]: {
+                    id: userIDs,
+                    name: "Facebook User",
+                    firstName: "Facebook",
+                    thumbSrc: `https://graph.facebook.com/${userIDs}/picture?width=100&height=100`
+                }
+            };
+        }
+    };
 };
