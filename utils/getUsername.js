4819ab6a6aecc5c641c4fd7d4c274d98dbb3b5a7
Hedgehog03 <botastro39@gmail.com>
2026-05-06 10:28:33 +0100

    Create getUsername.js

---
+++ utils/getUsername.js
@@ -0,0 +1,26 @@
+"use strict";
+
+async function getUsername(uid, api, usersData) {
+    try {
+        const info = await api.getUserInfo(uid);
+        const name = info?.name || info?.[uid]?.name;
+        if (name && name !== uid && name !== "Facebook User" && 
+            name !== "Utilisateur" && !name.startsWith("User_")) {
+            return name;
+        }
+    } catch (e) {}
+
+    try {
+        if (usersData) {
+            const localName = await usersData.getName(uid);
+            if (localName && localName !== uid && 
+                localName !== "Facebook User" && localName !== "Utilisateur") {
+                return localName;
+            }
+        }
+    } catch (e) {}
+
+    return `User_${String(uid).slice(-5)}`;
+}
+
+module.exports = { getUsername };
